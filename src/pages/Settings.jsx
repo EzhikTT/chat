fb91c2e10e621deb38d0e0d44a9a07e91e1397eb
aleksandrpetruhin <sprf8611@gmail.com>
2022-10-31 19:45:30 +0300

    some change

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import Aside from '../components/settings/Aside.jsx'
 import Content from '../components/settings/Content.jsx'
 
-
-
 const Settings = () => {
     return <>
         <Aside/>
