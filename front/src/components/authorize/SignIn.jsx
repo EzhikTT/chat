e3a1c2c12114b834c7a1705bbf2da96565daedd9
aleksandrpetruhin <sprf8611@gmail.com>
2022-12-05 19:50:03 +0300

    up in lesson

--- front/src/components/authorize/SignIn.jsx
+++ front/src/components/authorize/SignIn.jsx
@@ -52,27 +52,25 @@ const SignIn = () => {
     }
 
     return <form onSubmit={event => submit(event)}>
-    {errors.length > 0 && errors.map((error, id) => <div key={`error_${id}`} style={{color: 'red'}}>{error}</div>)}
-    <label>
-        <span>name</span>
-        <input name="name" onChange={(event) => setName(event.target.value)} required/>
-    </label><br/>
-    <label>
-        <span>login</span>
-        <input name="login" onChange={(event) => setLogin(event.target.value)} required/>
-    </label><br/>
-    <label>
-        <span>email</span>
-        <input name="email" type="email" onChange={(event) => setEmail(event.target.value)} required/>
-    </label><br/>
-    <label>
-        <span>password</span>
-        <input name="password" type="password" onChange={(event) => setPass(event.target.value)} required/>
-    </label><br/>
-    <button>signin</button>
-
-    
-</form>
+        {errors.length > 0 && errors.map((error, id) => <div key={`error_${id}`} style={{color: 'red'}}>{error}</div>)}
+        <label>
+            <span>name</span>
+            <input name="name" onChange={(event) => setName(event.target.value)} required/>
+        </label><br/>
+        <label>
+            <span>login</span>
+            <input name="login" onChange={(event) => setLogin(event.target.value)} required/>
+        </label><br/>
+        <label>
+            <span>email</span>
+            <input name="email" type="email" onChange={(event) => setEmail(event.target.value)} required/>
+        </label><br/>
+        <label>
+            <span>password</span>
+            <input name="password" type="password" onChange={(event) => setPass(event.target.value)} required/>
+        </label><br/>
+        <button>signin</button>
+    </form>
 }
 
 export default SignIn
