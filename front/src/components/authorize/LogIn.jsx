d545dd4b4c4951e0d78a8fc071a1986478c9e5e0
aleksandrpetruhin <sprf8611@gmail.com>
2022-12-09 19:28:03 +0300

    test

--- front/src/components/authorize/LogIn.jsx
+++ front/src/components/authorize/LogIn.jsx
@@ -5,6 +5,7 @@ import { setToken } from '../../store/main'
 import { useNavigate } from "react-router-dom";
 
 const LogIn = () => {
+    
     const [login, setLogin] = useState('')
     const [password, setPassword] = useState('')
 
